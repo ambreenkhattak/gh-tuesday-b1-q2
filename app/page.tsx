5e649e38a7e9a403bd29e64ddd1eab7ba7438c3e
ambreenkhattak <ambreenktk202@gmail.com>
2025-01-12 11:39:17 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-blue-600 text-7xl text-center text-white">Hello World!</h1>
+      <p className="text-center text-lg text-purple-950 bg-pink-300">First class of Quarter 2</p>
+    </div>
+  )
+}
